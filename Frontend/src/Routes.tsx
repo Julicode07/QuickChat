584cf70b8eb7902e7f1d0620022cc2da45ae31f3
JuliCode07 <Julicode07@gmail.com>
2025-02-27 21:03:47 -0500

    Enhance chat styles

--- Frontend/src/Routes.tsx
+++ Frontend/src/Routes.tsx
@@ -1,3 +1,4 @@
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -28,48 +28,52 @@ function Homepage() {
             {loading ? <Loader /> :
                 <Background>
                     <div className="relative z-50 flex h-screen">
-                        <aside className="flex flex-col gap-2 w-1/4 text-white">
-                            <div className="flex items-center gap-1 pt-4 px-4">
-                                <img src="/quickchat.webp" className="w-10 h-10" alt="Logo Quick Chat" />
-                                <h2 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-600 cursor-pointer">Quick Chat</h2>
+                        <aside className="flex flex-col gap-1 w-1/4 text-white">
+                            <div className="flex items-center gap-1 pt-4 pb-2 px-4 border-b-2 border-slate-900">
+                                <img src="/quickchat.webp" className="w-8 h-8" alt="Logo Quick Chat" />
+                                <h2 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-600 cursor-pointer">Quick Chat</h2>
                             </div>
-                            <div className="sticky top-0 bg-transparent backdrop-blur-xl px-5">
-                                <div className="space-y-2">
-                                    <h1 className="text-xl font-semibold">Chats</h1>
-                                    <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                            <div>
+                                {/* SEACRH INPUT */}
+                                <div className="sticky top-0 bg-transparent backdrop-blur-xl px-5 pb-3">
+                                    <div className="space-y-2">
+                                        <h1 className="text-xl font-semibold">Chats</h1>
+                                        <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                                    </div>
                                 </div>
-                            </div>
-                            <ul className="max-h-[calc(100vh-10rem)] overflow-y-auto">
-                                {results.length > 0 ? (
-                                    results.map((chat) => (
-                                        <motion.div
-                                            key={chat.id}
-                                            className="flex gap-2 px-4 py-3 hover:bg-gray-800 cursor-pointer font-normal"
-                                            initial={{ opacity: 0, y: 50 }}
-                                            animate={{ opacity: 1, y: 0 }}
-                                            exit={{ opacity: 0, y: -50 }}
-                                            transition={{ duration: 0.3 }}
-                                        >
-                                            <div className="flex items-center justify-center bg-gray-300 h-12 w-12 text-gray-500 rounded-full ">
-                                                <i className={`${chat.img} text-2xl`}></i>
-                                            </div>
-                                            <div className="flex flex-col justify-center">
-                                                <p className="text-base/tight font-normal">{chat.name}</p>
-                                                <div className="flex gap-1">
-                                                    <span className="flex items-center justify-center">
-                                                        <MessageStatus status="delivered" isSeen={false} />
-                                                    </span>
-                                                    <div className="flex items-end">
-                                                        <p className="pt-1 text-sm/tight text-gray-500 font-semibold">{chat.message}</p>
+                                <ul className="max-h-[calc(100vh-10rem)] overflow-y-auto overflow-x-hidden w-full">
+                                    {/*  CHATS */}
+                                    {results.length > 0 ? (
+                                        results.map((chat) => (
+                                            <motion.div
+                                                key={chat.id}
+                                                className="flex gap-2 px-4 py-2 hover:bg-gray-800 cursor-pointer font-normal w-full"
+                                                initial={{ opacity: 0, y: 50 }}
+                                                animate={{ opacity: 1, y: 0 }}
+                                                exit={{ opacity: 0, y: -50 }}
+                                                transition={{ duration: 0.3 }}
+                                            >
+                                                <div className="flex items-center justify-center bg-gray-300 h-12 w-12 text-gray-500 rounded-full ">
+                                                    <i className={`${chat.img} text-2xl`}></i>
+                                                </div>
+                                                <div className="flex flex-col justify-center w-[calc(100%-4.5rem)]">
+                                                    <p className="text-base font-normal truncate">{chat.name}</p>
+                                                    <div className="flex gap-1 w-full">
+                                                        <span className="flex items-center justify-center">
+                                                            <MessageStatus status="delivered" isSeen={false} />
+                                                        </span>
+                                                        <div className="flex items-end w-full">
+                                                            <p className="pt-1 text-sm text-gray-500 font-semibold truncate" title={chat.message}>{chat.message}</p>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </motion.div>
-                                    ))
-                                ) : (
-                                    <li className="mb-3 p-2 ml-3 font-semibold">No se encontraron resultados.</li>
-                                )}
-                            </ul>
+                                            </motion.div>
+                                        ))
+                                    ) : (
+                                        <li className="mb-3 p-2 ml-3 font-semibold">No tienes chats recientes</li>
+                                    )}
+                                </ul>
+                            </div>
                         </aside>
 
                         <div className="w-3/4 bg-gray-700/20 p-6 text-white">
